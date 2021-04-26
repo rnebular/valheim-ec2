673446d28c5dd83892a1679bd62fafb9eec67d35
rnebular <jtbyrum@gmail.com>
2021-04-26 05:49:45 -0700

    adding timestamp to shutdown

--- shutdown.ps1
+++ shutdown.ps1
@@ -3,5 +3,6 @@ $date = Get-Date
 Write-Host "$date - Waiting for 20 minutes to fully wait for auto-save, then shut down the server."
 Start-Sleep 1200
 
-Write-Host "Shutting down Valhiem instance..."
+$date = Get-Date
+Write-Host "$date - Shutting down Valhiem instance..."
 aws ec2 stop-instances --instance-ids i-079dbf837d3ea8d70
